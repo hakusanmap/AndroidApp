c9314f5f1fb6d7985f25f045d54bbfa7859f030b
yoshiki0309 <b1816606@planet.kanazawa-it.ac.jp>
2020-12-09 11:01:41 +0900

    top padding delete

--- app/src/main/java/io/keiji/sample/hakusan_mapapp/MapActivity.kt
+++ app/src/main/java/io/keiji/sample/hakusan_mapapp/MapActivity.kt
@@ -1,19 +1,19 @@
 package io.keiji.sample.hakusan_mapapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -24,7 +24,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.fragment_map)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         setContentView(R.layout.activity_main)
@@ -32,8 +32,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_information, R.id.navigation_map, R.id.navigation_movie))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_information, R.id.navigation_map, R.id.navigation_movie
+            )
+        )
         //Navigationを利用するための宣言
         setupActionBarWithNavController(navController, appBarConfiguration)
         //BottomBar作成
@@ -43,9 +46,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val sydney = CameraPosition.Builder()
+            .target(LatLng(-33.87365, 151.20689)).zoom(15.5f)
+            .bearing(0f).tilt(25f).build()
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(sydney))
+/*
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);*/
     }
 }
