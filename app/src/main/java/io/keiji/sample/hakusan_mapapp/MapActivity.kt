f31c9a8d8e17385efdc288d6b2adbf88da28faee
yoshiki0309 <b1816606@planet.kanazawa-it.ac.jp>
2020-12-16 16:14:27 +0900

    map fix

--- app/src/main/java/io/keiji/sample/hakusan_mapapp/MapActivity.kt
+++ app/src/main/java/io/keiji/sample/hakusan_mapapp/MapActivity.kt
@@ -1,31 +1,21 @@
 package io.keiji.sample.hakusan_mapapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
+class MapsActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_map)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
@@ -33,27 +23,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_information, R.id.navigation_map, R.id.navigation_movie
-            )
+                setOf(
+                        R.id.navigation_information, R.id.navigation_map, R.id.navigation_movie
+                )
         )
         //Navigationを利用するための宣言
         setupActionBarWithNavController(navController, appBarConfiguration)
         //BottomBar作成
         navView.setupWithNavController(navController)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        val sydney = CameraPosition.Builder()
-            .target(LatLng(-33.87365, 151.20689)).zoom(15.5f)
-            .bearing(0f).tilt(25f).build()
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(sydney))
-/*
-        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        mMap.getUiSettings().setMapToolbarEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setCompassEnabled(true);*/
     }
 }
--- app/src/main/java/io/keiji/sample/hakusan_mapapp/ui/map/MapViewModel.kt
+++ app/src/main/java/io/keiji/sample/hakusan_mapapp/ui/map/MapViewModel.kt
@@ -1,13 +1,28 @@
 package io.keiji.sample.hakusan_mapapp.ui.map
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapViewModel : ViewModel() {
+class MapViewModel : ViewModel(), OnMapReadyCallback {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Map Fragment"
+    private lateinit var mMap: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        val sydney2 = LatLng(-34.0, 140.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.addMarker(MarkerOptions().position(sydney2).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
-    val text: LiveData<String> = _text
 }
